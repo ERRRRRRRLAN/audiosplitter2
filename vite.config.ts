25e3992df11e0251e3354c4870f2dd8f2f0d3e73
Faza-SAPUTRA <fatirzaidan.p@gmail.com>
2026-01-20 20:35:12 +0700

    Fix: Switch to relative paths for GH Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/audiosplitter2/', // Updated to match repo name
+  base: './',
   server: {
     headers: {
       "Cross-Origin-Embedder-Policy": "require-corp",
